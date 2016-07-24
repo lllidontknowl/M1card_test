ea5f1c0255ff6d66f0a7b2b865aa2dbd383095c9
lllidontknowl <wuken840325@gmail.com>
2016-07-24 16:15:34 +0800

    first version
    some bug there

---
+++ m1card_test/m1card_test/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace m1card_test
+{
+    [Activity(Label = "m1card_test", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate
+            {
+                Intent read_intent = new Intent(this, typeof(m1_read));
+                this.StartActivity(read_intent);
+                Finish();
+            };
+        }
+    }
+}
+
---
+++ m1card_test/m1card_test/m1_read.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Nfc;
+
+namespace m1card_test
+{
+    [Activity(Label = "m1_read",  Icon = "@drawable/icon", LaunchMode = Android.Content.PM.LaunchMode.SingleTop)]
+    [IntentFilter(
+    new[] {NfcAdapter.ActionTechDiscovered}, 
+    Categories = new[] {Intent.CategoryDefault,})]
+    //[MetaData("android.nfc.action.TECH_DISCOVERED", Resource = "@xml/tech_list")]
+    public class m1_read : Activity
+    {
+        TextView mTV;
+        PendingIntent mPendingIntent;
+        IntentFilter ndefDetected;
+        IntentFilter[] intentF;
+        String[][] techLists;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.m1_read);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Intent Myintent = new Intent(this, GetType());
+            Myintent.AddFlags(ActivityFlags.SingleTop);
+            mPendingIntent = PendingIntent.GetActivity(this, 0, Myintent, 0);
+            ndefDetected = new IntentFilter(NfcAdapter.ActionTechDiscovered);
+         
+            intentF = new IntentFilter[] { ndefDetected };
+            techLists = new string[][] {new string[] {typeof(Android.Nfc.Tech.NfcA).FullName} };
+
+            Button button = FindViewById<Button>(Resource.Id.Back_Button);
+            mTV = FindViewById<TextView>(Resource.Id.textview);
+            button.Click += delegate
+            {
+                Intent main_intent = new Intent(this, typeof(MainActivity));
+                this.StartActivity(main_intent);
+                Finish();
+            };
+
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            NfcManager manager = (NfcManager)GetSystemService(NfcService);
+            manager.DefaultAdapter.DisableForegroundDispatch(this);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            NfcManager manager = (NfcManager)GetSystemService(NfcService);
+            mTV.Text = "Resume";
+            manager.DefaultAdapter.EnableForegroundDispatch(this, mPendingIntent, intentF,techLists);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            mTV.Text = "OnNewIntent";
+        }
+    }
+}
